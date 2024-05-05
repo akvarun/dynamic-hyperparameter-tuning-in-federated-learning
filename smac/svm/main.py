af79e75e942340683336134e66b97ef501da33a5
akvarun <akvarun08@gmail.com>
2024-05-05 23:57:47 +0530

    perf: Removed use of temporary lists

--- smac/svm/main.py
+++ smac/svm/main.py
@@ -8,39 +8,27 @@ DATA_DIR = "./data/"
 
 
 def load_cifar_10_train():
-    # Pre-allocate memory
-    num_samples = 50000  # Total number of samples in CIFAR-10 training set
-    image_size = 3072  # Size of each image (32x32x3)
+    num_samples = 50000
+    image_size = 3072
+    batch_size = 10000
     X = np.empty((num_samples, image_size), dtype=np.uint8)
     y = np.empty(num_samples, dtype=np.uint8)
-    index = 0  # Index to keep track of the current position to append
+    index = 0
     for file_name in os.listdir(DATA_DIR):
         if file_name.startswith("data_batch_"):
             file_path = os.path.join(DATA_DIR, file_name)
             with open(file_path, "rb") as file:
                 batch_data = pickle.load(file, encoding="bytes")
-                batch_images = batch_data[b"data"]
-                batch_labels = np.array(batch_data[b"labels"], dtype=np.uint8)
-                batch_size = len(batch_labels)
-                X[index : index + batch_size] = batch_images
-                y[index : index + batch_size] = batch_labels
+                X[index : index + batch_size] = batch_data[b"data"]
+                y[index : index + batch_size] = batch_data[b"labels"]
                 index += batch_size
-                print("Added", file_path)
     return X, y
 
 
 def load_cifar_10_test():
-    # Pre-allocate memory
-    num_samples = 10000  # Total number of samples in CIFAR-10 test set
-    image_size = 3072  # Size of each image (32x32x3)
-    X = np.empty((num_samples, image_size), dtype=np.uint8)
-    y = np.empty(num_samples, dtype=np.uint8)
     file_path = os.path.join(DATA_DIR, "test_batch")
     with open(file_path, "rb") as file:
         batch_data = pickle.load(file, encoding="bytes")
-        batch_images = batch_data[b"data"]
-        batch_labels = np.array(batch_data[b"labels"], dtype=np.uint8)
-        batch_size = len(batch_labels)
-        X[:batch_size] = batch_images
-        y[:batch_size] = batch_labels
+        X = np.array(batch_data[b"data"])
+        y = np.array(batch_data[b"labels"])
     return X, y
